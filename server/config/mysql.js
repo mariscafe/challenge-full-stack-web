80278aad2f979dafefd97a685e66e97174fa00de
Maricelia Ferreira <mariscafe@gmail.com>
2021-07-23 19:28:18 -0300

    build: install and configure dotenv

--- server/config/mysql.js
+++ server/config/mysql.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'banco-admin',
-  database: 'grupoa'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 })
 
 connection.on('error', function (err) {
--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
 const studentsRouter = require('./routes/api/students.js')
@@ -18,5 +20,5 @@ app.get('/', (req, res) => {
   })
 })
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Server started on port ${port}`))
